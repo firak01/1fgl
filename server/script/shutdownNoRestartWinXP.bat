56eef3ad1e6a59c568115f3b8de1fdf24b40668b
firak01 <firak01@t-online.de>
2019-02-10 08:03:20 +0100

    HannbalDEV05VM
    Signed-off-by: firak01 <firak01@t-online.de>

---
+++ server/script/shutdownNoRestartWinXP.bat
@@ -0,0 +1,5 @@
+cd\
+cd server\script
+REM start /wait onShutdown.bat
+.\shutdown\shutdown.exe /L /T:30 "MSG:Rechner wird heruntergefahren."
+exit
---
+++ server/script/shutdownRestartWinXP.bat
@@ -0,0 +1,5 @@
+cd\
+cd server\script
+REM start /wait onShutdown.bat
+.\shutdown\shutdown.exe /L /R /T:30 "Rechner wird heruntergefahren und NEU gestartet."
+exit
