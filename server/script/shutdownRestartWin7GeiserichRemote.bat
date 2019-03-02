ede97889114f7db27df4626a03cbe4aacc5eae80
firak01 <firak01@t-online.de>
2019-03-02 15:31:27 +0100

    UGAKI-SRV: Geiserich remote herunterfahren Signed-off-by: firak01 <firak01@t-online.de>

--- server/script/shutdownRestartWin7GeiserichRemote.bat
+++ server/script/shutdownRestartWin7GeiserichRemote.bat
@@ -1,6 +1,14 @@
 ﻿REM FGL 20190201:
 REM Batch zum Runterfahren und Neustarten von Win7 Rechnern REMOTE
 REM zwischen den Parametern /t: 720 muss nach dem Doppelpunkt ein Leerzeichen stehen
+
+
+REM Voraussetzungen
+REM 1. Service/Dienst RemoteRegistry auf beiden beteiligten Rechnern aktiviert und gestartet
+REM 2. Auf beiden beteiligten Rechnern in der Windows Registry folgenden Eintrag eränzt:
+REM FGL 20190201:
+REM Batch zum Runterfahren und Neustarten von Win7 Rechnern REMOTE
+REM zwischen den Parametern /t: 720 muss nach dem Doppelpunkt ein Leerzeichen stehen
 REM Ein Abbrechen des Remote Shutdowns ist auch remote möglich
 
 REM Voraussetzungen
@@ -8,13 +16,14 @@ REM 1. Service/Dienst RemoteRegistry auf beiden beteiligten Rechnern aktiviert u
 REM 2. Auf beiden beteiligten Rechnern in der Windows Registry folgenden Eintrag ergänzt, bzw. das Abstellen der Rechtefilterung von der UAC ist notwendig:
 REM    Abhilfe schafft der Eintrag eines Schlüssels in die Registrierdatenbank des verwalteten PCs, so dass die Rechte des lokalen Admins auch über das Netz durchgereicht werden. Dazu gibt man am einfachsten in einer Eingabeaufforderung mit Adminstratorrechten den Befehl
 REM    reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
+REM  
 REM    
-shutdown /f /s -m \\Ugaki-SRV /t: 720 /c "Remote Shutdown des Hostsystems. Dringend alle Server runterfahren."
+shutdown /f /r -m \\Geiserich /t: 30 /c "Remote shutdown des Rechners. Dringend alle Server runterfahren."
 pause
 exit
 
 REM Das ist so nicht möglich.....
 REM falls noch spezielles erledigt werden muss. Diese Datei umbenennen z.B. nach shutdownRestartScipio.bat und dann jetzt das Spezialscript verwenden
 REM cd\
-REM cd server\script
+REM cd 1fgl\server\script
 REM start /wait onShutdownScipio.bat
