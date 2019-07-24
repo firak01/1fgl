960914ff5f3f821bb2fdf123db1e78f9d54700bf
firak01 <firak01@t-online.de>
2019-07-24 20:06:19 +0200

    Narses03 - WakeOnLAN Script Narses03 - Anpassung des Scipio Startup Script an Win7
    Signed-off-by: firak01 <firak01@t-online.de>

---
+++ server/script/onStartupNarses03.bat
@@ -0,0 +1,35 @@
+@echo offf
+REM Starte nach dem Einschalten alle angeschlossenen Rechner
+REM Win7 Version: Hier gibt es den sleep Befehl nicht mehr. 
+REM               Verwende timeout
+REM               Sleep Befehl kann über "Windows Resource Kit Tools" installiert werden.
+
+
+REM #######################################################
+echo WARTE ERST EINIGE ZEIT -  REchner SOLL FERTIG SEIN UND NETZWERK SOLL AKTIV SEIN
+timeout 240
+ 
+echo ################################################################
+echo VERSUCHE DEN RECHNER ALARICH ZU STARTEN
+timeout 30
+REM call C:\1fgl\server\WakeOnLAN\mc-wol\wolAlarichFGL.bat
+start C:\1fgl\server\WakeOnLAN\mc-wol\wolAlarichFGL.bat
+
+REM echo ################################################################
+REM echo VERSUCHE DEN RECHNER PATTON ZU STARTEN
+REM timeout 10
+REM call C:\1fgl\server\WakeOnLAN\mc-wol\wolPattonFGL.bat
+
+
+@echo on
+REM Fahre den Rechner wieder automatisch herunter
+echo ################################################################
+echo VERSUCHE DEN RECHNER SCIPIO WIEDER HERUNTERZUFAHREN
+timeout 30
+REM call C:\1fgl\server\script\shutdownNoRestartScipioHost.bat
+REM start C:\1fgl\server\script\shutdownNoRestartScipioHost.bat
+start C:\1fgl\server\script\shutdownNoRestartWin7.bat
+
+
+pause
+exit
