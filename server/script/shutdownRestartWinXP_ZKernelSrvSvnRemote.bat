dd3e76344e36277b0de977b39412fe0e735aa53d
firak01 <firak01@t-online.de>
2021-01-11 19:02:49 +0100

    ZKernelSrvSvnVM Shutdown-Remote Scripte
    Signed-off-by: firak01 <firak01@t-online.de>

---
+++ server/script/shutdownRestartWinXP_ZKernelSrvSvnRemote.bat
@@ -0,0 +1,7 @@
+﻿REM FGL: 20210107
+REM Batch zum Runterfahren und Neustarten von Win7 Rechnern REMOTE
+REM In den Shutdown Scripten ist beschrieben, welche Voraussetzungen erfüllt sein müssen.
+net use \\192.168.3.131 1fgl2fgl /USER:lindhaueradmin
+shutdown /f /r -m \\192.168.3.131 /t: 20 /c "Remote Neustart des Systems. Dringend alle Server runterfahren."
+pause
+exit
