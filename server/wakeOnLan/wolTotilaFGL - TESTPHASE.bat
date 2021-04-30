15bfc25a088f475d12aae9db677bfc2e7f68b9d9
firak01 <firak01@t-online.de>
2021-04-30 11:26:54 +0200

    Totila
    Signed-off-by: firak01 <firak01@t-online.de>

--- server/wakeOnLan/wolTotilaFGL - TESTPHASE.bat
+++
@@ -1,4 +0,0 @@
-REM IP Ansteuern /a 192.168.3.110
-REM MAC Adresse
-mc-wol 30:D0:42:15:1A:1F
-pause
--- server/wakeOnLan/wolTotilaFGL.bat
+++ server/wakeOnLan/wolTotilaFGL.bat
@@ -4,3 +4,4 @@ REM IP Adresse /a 192.168.3.110
 REM MAC Adresse
 REM Physikalische Adresse hier takagi:
 mc-wol 80:FA:5B:83:23:D0
+pause
