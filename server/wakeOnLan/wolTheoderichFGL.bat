ec8099cc0d3dde87d44b94bfa26d52348da5f656
firak01 <firak01@users.noreply.github.com>
2022-02-05 15:50:13 +0100

    TAKAGI-PRV
    Signed-off-by: firak01 <firak01@users.noreply.github.com>

---
+++ server/wakeOnLan/wolTheoderichFGL.bat
@@ -0,0 +1,10 @@
+REM IP Adresse /a 192.168.3.110
+
+REM 20220205: DAS FUNKTIONIERT NOCH NICHT !!!
+REM MAC Adresse
+REM In das Verzeichnis der EXE-Wechseln
+cd mc-wol
+
+REM Physikalische Adresse hier THEODERICH:
+mc-wol 50:7B:9D:D7:0A:F1
+pause
