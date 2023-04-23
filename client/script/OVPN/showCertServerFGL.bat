d1d7628425144fa7ccf1c549d9fb33b145aaf560
firak <firak01@t-online.de>
2023-04-23 07:48:46 +0200

    OVPN Tools
    Signed-off-by: firak <firak01@t-online.de>

---
+++ client/script/OVPN/showCertServerFGL.bat
@@ -0,0 +1,5 @@
+REM Funktioniert nur mit .crt Dateien nicht mit .csr .key
+REM openssl x509 -noout -text -in HANNIBALDEV04VM.csr
+cd\
+C:\Programme\OpenVPN\bin\openssl x509 -noout -text -in C:\1fgl\client\script\OVPN\HANNIBALDEV04VM_SERVER.crt
+pause
