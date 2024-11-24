8f3208ea54973d61c8b65af8352e62b4feb40392
firak01 <firak01@t-online.de>
2024-11-24 20:44:40 +0100

    WOL ERMANARICH - In Arbeit, funktioniert nicht
    Signed-off-by: firak01 <firak01@t-online.de>

---
+++ server/wakeOnLan/wolErmanarichFGL.bat
@@ -0,0 +1,14 @@
+REM IP Adresse /a 192.168.3.110
+
+REM 20220205: DAS FUNKTIONIERT NOCH NICHT !!!
+REM MAC Adresse
+REM In das Verzeichnis der EXE-Wechseln
+cd mc-wol
+
+REM Physikalische Adresse hier ERMANARICH:
+REM: Laut BIOS
+REM: mc-wol 54:E1:AD:5D:E9:06
+
+REM: Docking Station?
+mc-wol A0:AF:BD:16:00:F3
+pause
