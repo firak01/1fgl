59b792b43359479ead79b5ac8877c430ded5ea9c
firak01 <firak01@t-online.de>
2025-01-19 18:27:12 +0100

    WAKE ON LAN ERMANERICH
    Signed-off-by: firak01 <firak01@t-online.de>

--- server/wakeOnLan/wolErmanarichFGL.bat
+++ server/wakeOnLan/wolErmanarichFGL.bat
@@ -1,15 +1,15 @@
-REM IP Adresse /a 192.168.3.110
-
-REM 20220205: DAS FUNKTIONIERT NOCH NICHT !!!
 REM MAC Adresse
 REM In das Verzeichnis der EXE-Wechseln
 cd mc-wol
 
-REM Physikalische Adresse hier ERMANARICH:
-REM: Laut BIOS, und wenn Docking Stations Thunderbold angeschlossen und erlaubt ist.
-mc-wol 54:E1:AD:5D:E9:06
+REM Docking Station?
+REM mc-wol A0:AF:BD:16:00:F3
 
-REM: Docking Station?
-REM: mc-wol A0:AF:BD:16:00:F3
-REM: mc-wol 00:50:B6:A7:03:02
+REM Laut BIOS, Dockung Station THUNDERBOLD muss an sein!!!
+REM mc-wol 54:E1:AD:5D:E9:06
+
+REM 20250119 - Funktioniert mit der APP und mit dieser Datei, aber nur wg. der IP Adresse!!!
+REM Physikalische Adresse hier ERMANARICH:
+mc-wol 54:E1:AD:5D:E9:06 /a 192.168.3.164
+
 pause
