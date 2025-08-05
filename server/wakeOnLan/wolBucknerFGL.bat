28006489144b4907f01151858caf471722441ebe
firak01 <firak01@t-online.de>
2025-08-05 11:27:08 +0200

    WAKE ON LAN NEUER PC - BUCKNER
    Signed-off-by: firak01 <firak01@t-online.de>

---
+++ server/wakeOnLan/wolBucknerFGL.bat
@@ -0,0 +1,10 @@
+REM In das Verzeichnis der .exe wechseln
+
+REM IP Adresse /a 192.168.3.110
+REM MAC Adresse
+REM Physikalische Adresse hier takagi:
+
+REM In das Verzeichnis der EXE-Wechseln
+cd mc-wol
+mc-wol 10:FF:E0:AF:6D:10
+pause
